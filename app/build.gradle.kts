0a647ad32b7b4ccf6c0878fdd981ed2aa54b46de
Ruchi Mulik <mulikruchi07@gmail.com>
2025-12-21 00:40:13 +0530

    solved gradel error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,21 +35,27 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    useLibrary("wear-sdk")
     buildFeatures {
         compose = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
-    useLibrary("com.google.android.wear")
 }
 
 dependencies {
+    // Core
+    implementation ("androidx.core:core-ktx:1.13.1")
+    // Wear OS Compose
+    implementation ("androidx.wear.compose:compose-foundation:1.3.1")
+    // Splash screen
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+    // Lifecycle
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    // Debug
+    debugImplementation ("androidx.compose.ui:ui-tooling")
     implementation("androidx.wear.compose:compose-material:1.3.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.wear:wear:1.2.0")
-    implementation(libs.play.services.wearable)
     implementation(platform(libs.compose.bom))
     implementation(libs.ui)
     implementation(libs.ui.graphics)
